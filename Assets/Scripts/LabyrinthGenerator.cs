966eca666693ebd4d70c32415691516763600476
adenzu <erencakar54@gmail.com>
2025-11-03 20:03:48 +0300

    make maze into real tilemap

--- Assets/Scripts/LabyrinthGenerator.cs
+++ Assets/Scripts/LabyrinthGenerator.cs
@@ -4,6 +4,15 @@ using UnityEngine.Tilemaps;
 public class LabyrinthGenerator : MonoBehaviour
 {
     [SerializeField]
+    private Tilemap m_tilemap;
+
+    [SerializeField]
+    private TileBase m_wallTile;
+
+    [SerializeField]
+    private TileBase m_pathTile;
+
+    [SerializeField]
     private bool m_limitBranch = false;
 
     [SerializeField, Min(1)]
@@ -11,9 +20,6 @@ public class LabyrinthGenerator : MonoBehaviour
 
     [SerializeField, Min(0)]
     private Vector2Int m_size;
-
-    [SerializeField]
-    private Tilemap m_tilemap;
 
     private uint[,] m_cells;
 
@@ -23,6 +29,7 @@ public class LabyrinthGenerator : MonoBehaviour
     {
         m_cells = new uint[m_size.x, m_size.y];
         Generate(0, 0, m_branchLengthLimit);
+        DrawActualCells();
     }
 
     private void Generate(int x, int y, int length)
@@ -68,46 +75,69 @@ public class LabyrinthGenerator : MonoBehaviour
         return (PathDirection)(8 / (uint)direction);
     }
 
-    private bool IsTileWall(int x, int y)
+    private bool IsVirtualCellWall(int x, int y)
     {
         int cellX = x / m_actualToVirtualCellRatio;
         int cellY = y / m_actualToVirtualCellRatio;
 
         bool[,] lookUpTable = new bool[m_actualToVirtualCellRatio, m_actualToVirtualCellRatio]
         {
-            {true, !CellContainsDirection(cellX, cellY, PathDirection.Down), !CellContainsDirection(cellX, cellY, PathDirection.Down), true},
-            {!CellContainsDirection(cellX, cellY, PathDirection.Left), m_cells[cellX, cellY] == 0, m_cells[cellX, cellY] == 0, !CellContainsDirection(cellX, cellY, PathDirection.Right)},
-            {!CellContainsDirection(cellX, cellY, PathDirection.Left), m_cells[cellX, cellY] == 0, m_cells[cellX, cellY] == 0, !CellContainsDirection(cellX, cellY, PathDirection.Right)},
-            {true, !CellContainsDirection(cellX, cellY, PathDirection.Up), !CellContainsDirection(cellX, cellY, PathDirection.Up), true},
+            {true, !ActualCellContainsDirection(cellX, cellY, PathDirection.Down), !ActualCellContainsDirection(cellX, cellY, PathDirection.Down), true},
+            {!ActualCellContainsDirection(cellX, cellY, PathDirection.Left), m_cells[cellX, cellY] == 0, m_cells[cellX, cellY] == 0, !ActualCellContainsDirection(cellX, cellY, PathDirection.Right)},
+            {!ActualCellContainsDirection(cellX, cellY, PathDirection.Left), m_cells[cellX, cellY] == 0, m_cells[cellX, cellY] == 0, !ActualCellContainsDirection(cellX, cellY, PathDirection.Right)},
+            {true, !ActualCellContainsDirection(cellX, cellY, PathDirection.Up), !ActualCellContainsDirection(cellX, cellY, PathDirection.Up), true},
         };
 
         return lookUpTable[y % m_actualToVirtualCellRatio, x % m_actualToVirtualCellRatio];
     }
 
-    private bool CellContainsDirection(int x, int y, PathDirection direction)
+    private bool ActualCellContainsDirection(int x, int y, PathDirection direction)
     {
         return 0 != (m_cells[x, y] & ((uint)direction));
     }
 
-    void OnDrawGizmos()
+    private void SetTileWall(Vector3Int tilePosition)
+    {
+        m_tilemap.SetTile(tilePosition, m_wallTile);
+    }
+
+    private void SetTilePath(Vector3Int tilePosition)
+    {
+        m_tilemap.SetTile(tilePosition, m_pathTile);
+    }
+
+    private void SetTile(int x, int y)
+    {
+        Vector3Int tilePosition = m_tilemap.WorldToCell(transform.position + new Vector3(x, y, 0));
+        const int m_actualToVirtualCellRatioHalf = m_actualToVirtualCellRatio / 2;
+        if (IsVirtualCellWall(m_actualToVirtualCellRatioHalf * x, m_actualToVirtualCellRatioHalf * y))
+        {
+            SetTileWall(tilePosition);
+        }
+        else
+        {
+            SetTilePath(tilePosition);
+        }
+    }
+
+    private void DrawActualCells()
     {
         if (m_cells == null) return;
+
         for (int x = 0; x < m_size.x * m_actualToVirtualCellRatio; x += 2)
         {
             for (int y = 0; y < m_size.y * m_actualToVirtualCellRatio; y += 2)
             {
-                Gizmos.color = IsTileWall(x, y) ? Color.black : Color.white;
-                Gizmos.DrawCube(m_tilemap.GetCellCenterWorld(m_tilemap.WorldToCell(transform.position + new Vector3(x / 2, y / 2, 0))), Vector3.one);
+                SetTile(x / 2, y / 2);
             }
-            Gizmos.color = Color.black;
-            Gizmos.DrawCube(m_tilemap.GetCellCenterWorld(m_tilemap.WorldToCell(transform.position + new Vector3(x / 2, m_size.y * m_actualToVirtualCellRatio / 2, 0))), Vector3.one);
+            SetTileWall(m_tilemap.WorldToCell(transform.position + new Vector3(x / 2, m_size.y * m_actualToVirtualCellRatio / 2, 0)));
         }
+
         for (int y = 0; y < m_size.y * m_actualToVirtualCellRatio; y += 2)
         {
-            Gizmos.color = Color.black;
-            Gizmos.DrawCube(m_tilemap.GetCellCenterWorld(m_tilemap.WorldToCell(transform.position + new Vector3(m_size.x * m_actualToVirtualCellRatio / 2, y / 2, 0))), Vector3.one);
+            SetTileWall(m_tilemap.WorldToCell(transform.position + new Vector3(m_size.x * m_actualToVirtualCellRatio / 2, y / 2, 0)));
         }
-        Gizmos.color = Color.black;
-        Gizmos.DrawCube(m_tilemap.GetCellCenterWorld(m_tilemap.WorldToCell(transform.position + new Vector3(m_size.x * m_actualToVirtualCellRatio / 2, m_size.y * m_actualToVirtualCellRatio / 2, 0))), Vector3.one);
+
+        SetTilePath(m_tilemap.WorldToCell(transform.position + new Vector3(m_size.x * m_actualToVirtualCellRatio / 2, m_size.y * m_actualToVirtualCellRatio / 2, 0)));
     }
 }
